44f4443c6c2854d08c671252e2608a3ee07286a2
Ritzoosk <knecht.ted@gmail.com>
2021-02-17 09:31:55 -0800

    Challenge 5

--- code-challenges/challenges-02.test.js
+++ code-challenges/challenges-02.test.js
@@ -90,6 +90,10 @@ console.log(people[1].isAuthor) prints true
 
 const setStatusAsAuthor = (people) => {
   // Solution code here...
+  people.forEach(person => {
+    person.isAuthor = true;
+  });
+
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -157,7 +161,7 @@ xdescribe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should add a property to every object in an array', () => {
     const a = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }, { fullName: 'Kurt Vonnegut' }];
     setStatusAsAuthor(a);
